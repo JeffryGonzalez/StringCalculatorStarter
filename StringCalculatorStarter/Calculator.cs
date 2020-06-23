a9a87738594173cf22cb845eda5bb4556601b55d
Jeffry Gonzalez <jeff@hypertheory.com>
2020-06-23 14:34:25 -0400

    Add project files.

---
+++ StringCalculatorStarter/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorStarter
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            return -1;
+        }
+    }
+}
---
+++ StringCalculatorStarter/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StringCalculatorStarter
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void EmptyStringReturnsZero()
+        {
+            var calculator = new Calculator();
+            
+            var result = calculator.Add("");
+
+            Assert.Equal(0, result);
+        }
+    }
+}
